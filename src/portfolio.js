0433c2d118a87098a79dcfc3fed37ec8bf46172b
Daniel <danielkofi4@gmail.com>
2022-02-08 22:17:13 +0000

    project added

---
+++ src/portfolio.js
@@ -0,0 +1,67 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: '/',
+  title: 'DY.',
+};
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'DANIEL YEBOAH',
+  role: 'Front End Engineer',
+  description:
+    'Creative React Developer with over a years experience designing useful, approachable user interfaces. Knowledgeable on all aspects of Facebooks design best practices and emerging UI development techniques. Skilled at connecting exceptional assets with users via creative UI frameworks and careful user experience optimization.',
+  resume: 'https://drive.google.com/file/d/1nZz8INQqfP7VhfbgnOmWjqnE-UyKt6iZ/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/daniel-yeboah-b92447110/',
+    github: 'https://github.com/dyofficial',
+  },
+};
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Secret Santa',
+    description: 'Secret Santa matchmaking app for workers',
+    stack: ['Material UI', 'Lottie Files', 'React', 'Google OAUTH'],
+    sourceCode: 'https://github.com/dyofficial/xpay-sec-santa',
+    livePreview: 'https://xpay-sec-santa.vercel.app/',
+  },
+  {
+    name: 'DY Store',
+    description: 'An e-commerce store built with commerce js and stripe API',
+    stack: ['Material UI', 'Rest API', 'React'],
+    sourceCode: 'https://github.com/dyofficial/dy-store',
+    livePreview: 'https://dy-store.vercel.app/',
+  },
+  // {
+  //   name: 'Chrome',
+  //   description:
+  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+  //   stack: ['SASS', 'TypeScript', 'React'],
+  //   sourceCode: 'https://github.com',
+  //   livePreview: 'https://github.com',
+  // },
+];
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'Jest',
+];
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'danielkofi4@gmail.com',
+};
+
+export { header, about, projects, skills, contact };
